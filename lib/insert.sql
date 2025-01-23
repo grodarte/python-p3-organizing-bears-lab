cb9a5e0362e6802683dad4ddd1662e6a502a4fbc
Gabrielle <ms.grodarte@gmail.com>
2025-01-23 14:42:58 -0800

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "sweet", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "rowdy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "white", "sassy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "brown", "grumpy", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "black", "nice", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 12, "M", "white", "grouchy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 18, "F", "brown", "nice", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "black", "playful", True);
